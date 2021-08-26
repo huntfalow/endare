e62332322c537f906532674b7e71b1f4724e7180
Vincent <vincent_coutuer@yahoo.com>
2021-08-26 08:14:52 +0200

    Added endare code to github repo

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { NativeModules } from 'react-native';
+
+type DatalogicScannerType = {
+  testScan(success: boolean): Promise<string>;
+  scanOnce(): Promise<string>;
+  startScanning(): Promise<string>;
+  stopScanning(): void;
+};
+
+const { DatalogicScanner } = NativeModules;
+
+export default DatalogicScanner as DatalogicScannerType;
